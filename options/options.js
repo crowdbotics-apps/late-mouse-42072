83db92b1d38fa5854e10e614f9b9ae418aee0eb8
Crowdbotics <team@crowdbotics.com>
2023-07-10 19:37:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "late-mouse-42072",
+  url: "https://late-mouse-42072.botics.co",
+  api: "https://late-mouse-42072.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
